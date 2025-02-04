da416201872252a158174768150d626b45ef449f
henriquebaz33 <henriquebaz33@gmail.com>
2025-02-04 20:44:56 -0300

    fazendo progresso no aprendizado

---
+++ learning/web/web-modern/fundamentos/inicio/dados_3.js
@@ -0,0 +1,10 @@
+let idade = 33;
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+let estaChovendo = false // ou true
+console.log(typeof estaChovendo);
+
+console.log(typeof "Teste");
