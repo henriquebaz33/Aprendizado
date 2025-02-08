70414736fafccdd69740629391bd6cd04a7b86bf
henriquebaz33 <henriquebaz33@gmail.com>
2025-02-08 19:03:54 -0300

    novas alterações previstas, revisões e novos exercícios de python

--- learning/python/exercicios/ex026.py
+++ learning/python/exercicios/ex026.py
@@ -2,6 +2,4 @@ frase = str(input("Digite uma frase: ")).lower()
 
 print(f"A letra A apareceu {frase.count("a")} vezes na frase")
 print(f"A primeira letra A apareceu na posição {frase.strip().find("a") + 1}")
-
-
-# parei aqui
+print(f'A última letra A apareceu na posição {frase.strip().rfind("a") + 1}')
